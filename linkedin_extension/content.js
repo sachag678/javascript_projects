239aabe32aa3079fb71603ae4036c802df27f488
Sacha Gunaratne <sacha.gunaratne@gmail.com>
2018-08-08 00:53:26 -0400

    The script now finds a list of like buttons and then clicks the first one.

---
+++ linkedin_extension/content.js
@@ -0,0 +1,11 @@
+var x = document.getElementsByClassName("relative ember-view"); //gets the posts
+
+//console.log(x[4].innerText)  Get the text of the post.
+
+var z = document.getElementsByClassName("like-button button like feed-shared-social-action-bar__action-btn social-action-btn mr5 ember-view"); //get the buttons
+
+console.log(z)
+
+console.log(z[0]) 
+
+z[0].click() //clicks the first available like button.
